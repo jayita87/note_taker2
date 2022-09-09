cac0cb540d464abd616872154be3e3519612aaa2
Jayita Chowdhury <jayitachowdhury@Amits-MacBook-Pro.local>
2022-09-09 09:13:17 -0700

    initial commit

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const uuid = require('../../helpers/generateID');
 
 const router = require('express').Router();
+const notes = require('../db/db.json');
 
-    router.get("/notes", (req, res) => {
-        res.json(notes);
+router.get("/notes", (req, res) => {
+ console.log(notes);
+    res.json(notes);
 });
 
 router.post("/notes", (req, res) => {
@@ -13,24 +15,39 @@ router.post("/notes", (req, res) => {
     const { title, text, id } = req.body;
     if(title && text){
         let newNote = {
-            title: req.body.title,
-            text: req.body.text,
-            id: uuid(),
+            title: title,
+            text: text,
+            id: uuid()
         }
         notes.push(newNote);
 
-        fs.readFile("../db/db.json", "utf-8", (err, data) => {
+        fs.readFile("db/db.json", "utf-8", (err, data) => {
+            console.log(data);
             const parsedRes = JSON.parse(data);
-            parsedRes.push(newNote)
-            fs.writeFile("../db/db.json", JSON.stringify(parsedRes), err => {
+            parsedRes.push(newNote);
+            fs.writeFile("db/db.json", JSON.stringify(parsedRes), err => {
                 err ? console.error(err) : console.log("Success")
             })
         })
     }
 });
 
-router.delete('/notes/:id', (req, res) => {
-    noteDeleteNote(notes, req.params.id);
-    res.json(notes);
- })
+ router.delete('/notes/:id', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        let notes = JSON.parse(data);
+
+        notes = notes.filter(note => (note.id !== req.params.id));
+
+        fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Note deleted!');
+                res.json(notes);
+            }
+        });
+
+    });
+})
+
  module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -9,13 +9,13 @@ const getID = require("../helpers/generateID.js");
 const app = express();
 
 const PORT = process.env.port || 3001;
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
 
-app.use(express.static('public'));
+
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
